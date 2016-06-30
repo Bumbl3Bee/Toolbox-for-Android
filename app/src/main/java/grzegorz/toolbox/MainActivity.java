dc4473d6f75e8110ed4d3b1b15b6c8709eec4164
Bumbl3Bee <grzegorz.baczek.93@hotmail.com>
2016-07-01 01:30:37 +0200

    Added flashlight functionality

--- app/src/main/java/grzegorz/toolbox/MainActivity.java
+++ app/src/main/java/grzegorz/toolbox/MainActivity.java
@@ -1,13 +1,79 @@
 package grzegorz.toolbox;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button flashlightButton;
+    private Camera camera;
+    private boolean isFlashOn;
+    Camera.Parameters parameters;
+
+    private void getCamera() {
+        if (camera == null) {
+            try {
+                camera = Camera.open();
+                parameters = camera.getParameters();
+            } catch (RuntimeException e) {
+                Log.e("Camera Error.", e.getMessage());
+            }
+        }
+    }
+
+    private void turnFlashOn() {
+        if (!isFlashOn) {
+            if (camera == null || parameters == null) {
+                return;
+            }
+
+            parameters = camera.getParameters();
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(parameters);
+            camera.startPreview();
+            isFlashOn = true;
+
+        }
+    }
+
+    private void turnFlashOff() {
+        if (isFlashOn) {
+            if (camera == null || parameters == null) {
+                return;
+            }
+
+            parameters = camera.getParameters();
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            camera.setParameters(parameters);
+            camera.stopPreview();
+            isFlashOn = false;
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flashlightButton = (Button) findViewById(R.id.flashlightButton);
+
+        getCamera();
+
+        flashlightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isFlashOn) {
+                    turnFlashOff();
+                } else {
+                    turnFlashOn();
+                }
+            }
+        });
     }
+
 }
